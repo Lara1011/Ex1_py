f248f185432792a395486f7ab66b98ccbd79a703
Malak47 <malak.qeedan@hotmail.com>
2021-11-15 21:26:39 +0200

    creating elevator class

--- Ex1.py
+++ Ex1.py
@@ -1,2 +1,18 @@
 import pandas as pd
 import numpy as np
+
+class Elevator:
+    def __init__(self, Elevator):
+        self.getID = Elevator['_id']
+        self.getSpeed = Elevator['_speed']
+        self.getMinFloor = Elevator['_minFloor']
+        self.getMaxFloor = Elevator['_maxFloor']
+        self.getTimeForClose = Elevator['_closeTime']
+        self.getTimeForOpen = Elevator['_openTime']
+        self.getStartTime = Elevator['_startTime']
+        self.getStopTime = Elevator['_stopTime']
+        self.elev = []
+
+
+
+
