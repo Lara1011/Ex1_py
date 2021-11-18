106d74bdf66045f7400a9f84e4d52573976319cd
Malak47 <malak.qeedan@hotmail.com>
2021-11-19 01:27:51 +0200

    finished coding, fixing bugs

--- Elevator.py
+++ Elevator.py
@@ -16,5 +16,13 @@ class Elevator:
         self.call = []
 
     def __str__(self):
-        string = "ID: " , self.getID , ", Speed: " , self.getSpeed, ", Min floor: " ,self.getMinFloor, ", Max floor: " ,self.getMaxFloor, ", Time for close: " ,self.getTimeForClose ,", Time for open: " , self.getTimeForOpen,
-        ", Start time: " ,self.getStartTime, ", Stop time: " , self.getStopTime
+        string = "ID: " , self.getID , ", Speed: " , self.getSpeed, ", Min floor: " ,self.getMinFloor, ", Max floor: " ,self.getMaxFloor, ", Time for close: " ,self.getTimeForClose ,", Time for open: " , self.getTimeForOpen,\
+                 ", Start time: " ,self.getStartTime, ", Stop time: " , self.getStopTime
+
+    def travelTime(self, src, dest):
+        time = self.getTimeForClose + self.getStartTime + (abs(src - dest) / self.getSpeed) \
+               + self.getStopTime + self.getTimeForOpen
+        return time
+
+
+
--- Ex1.py
+++ Ex1.py
@@ -8,7 +8,8 @@ import json
 
 def LoadCalls(path):
     try:
-        calls = pd.read_csv(path)
+        calls = pd.read_csv(path,header=None)
+        return calls
     except:
         print("ERROR: This is not a CSV file !")
 
@@ -19,15 +20,66 @@ def LoadBuilding(path):
             my_building = Building(build)
             for my_elev in build['_elevators']:
                 my_building.elev.append(Elevator(my_elev))
+            #my_building.elev.sort(key=lambda x: x[1])
+        return my_building
     except:
         print("ERROR: This is not a JSON file !")
 
-def LoadOutput(call_row, path):
-    with open(path, 'a+', newline='') as write_obj:
-        csv_writer = writer(write_obj)
-        csv_writer.writerow(call_row)
+def LoadOutput(index, path, row):
+    path.loc[row, '5'] = index
+    return path
 
-def Time(Elevator, src, dest):
-    time = Elevator.getTimeForClose + Elevator.getStartTime + (abs(src-dest)/Elevator.getSpeed) \
-           + Elevator.getStopTime + Elevator.getTimeForOpen
-    return time
+def allocateElevator(path1, path2, path3):
+    myBuilding = LoadBuilding(path1)
+    myCalls = LoadCalls(path2)
+    myOut = LoadCalls(path2)
+
+    myOut['difference'] = abs(myOut[2]-myOut[3])
+    myOut = myOut.sort_values(['difference'], ascending=True)
+    numOfElev = len(myBuilding.elev)
+    partition = int(len(myOut) / numOfElev)
+    if numOfElev == 1:
+        for i in myOut.index:
+            myOut[5] = myBuilding.elev[0].getID
+    else:
+        sortElevList = myBuilding.elev
+        start = 0
+        stop = len(myBuilding.elev)
+        for i in range(0,len(myOut),partition):
+            if start<stop:
+                myOut.loc[i:i+partition -1 ,5] = sortElevList[start].getID
+            start = start + 1
+       # i = i + partition
+    myOut = myOut.drop(['difference'],axis=1)
+    j=0
+    k=partition
+    for i in myOut:
+        while(i<k):
+            myBuilding.elev[j].call.append(myOut[2].iloc[i])
+            myBuilding.elev[j].call.append(myOut[3].iloc[i])
+            i = i+1
+        k = k+partition
+        j = j+1
+        if len(myBuilding.elev)==j:
+            break
+    myOut = myOut.sort_values([1])
+    myOut.to_csv(path3, mode='a')
+'''''
+    # path 3 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    if len(myBuilding.elev)==1:
+        for i in myCalls:
+            myOut = LoadOutput(myBuilding.elev.getID, myOut, i)
+    else:
+        for i in myCalls:
+            minTime = []
+            for j in len(myBuilding.elev):
+
+def sumTillNextCall(elevator, call):
+    # check this copy() if works !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    copyList = elevator.call.copy()
+    calTime = 0
+    for i in copyList:
+'''''
+
+if __name__ == '__main__':
+    allocateElevator(r'C:\Users\malak\OneDrive\Desktop\New folder\B2.json', r'C:\Users\malak\OneDrive\Desktop\New folder\Calls_a.csv', r'C:\Users\malak\OneDrive\Desktop\New folder\output.csv')
